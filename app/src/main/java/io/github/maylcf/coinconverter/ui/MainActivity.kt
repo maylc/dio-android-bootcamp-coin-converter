9242d4ccc524ebe71ba17ea8a2fbf1605a7b1562
Mayara Felix <maylc.dev@gmail.com>
2021-10-11 00:39:53 -0400

    Handle request on main activity.

--- app/src/main/java/io/github/maylcf/coinconverter/ui/MainActivity.kt
+++ app/src/main/java/io/github/maylcf/coinconverter/ui/MainActivity.kt
@@ -1,15 +1,22 @@
 package io.github.maylcf.coinconverter.ui
 
 import android.os.Bundle
+import android.util.Log
 import android.widget.ArrayAdapter
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.widget.doAfterTextChanged
+import io.github.maylcf.coinconverter.core.extensions.createDialog
+import io.github.maylcf.coinconverter.core.extensions.createProgressDialog
 import io.github.maylcf.coinconverter.data.model.Coin
 import io.github.maylcf.coinconverter.databinding.ActivityMainBinding
+import io.github.maylcf.coinconverter.presentation.MainViewModel
+import org.koin.androidx.viewmodel.ext.android.viewModel
 
 class MainActivity : AppCompatActivity() {
 
+    private val dialog by lazy { createProgressDialog() }
     private val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
+    private val viewModel by viewModel<MainViewModel>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -17,6 +24,23 @@ class MainActivity : AppCompatActivity() {
 
         bindAdapters()
         bindListeners()
+
+        // Test Request
+        viewModel.getExchangeValue("USD-BRL")
+
+        viewModel.state.observe(this) {
+            when (it) {
+                MainViewModel.State.Loading -> dialog.show()
+                is MainViewModel.State.Error -> {
+                    dialog.dismiss()
+                    createDialog { setMessage(it.throwable.message) }.show()
+                }
+                is MainViewModel.State.Success -> {
+                    dialog.dismiss()
+                    Log.e("TAG", "onCreate: ${it.value}")
+                }
+            }
+        }
     }
 
     private fun bindAdapters() {
