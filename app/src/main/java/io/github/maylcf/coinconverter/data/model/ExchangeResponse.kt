87743bee5bd936ce0c44dd0350f78156a257fc60
Mayara Felix <maylc.dev@gmail.com>
2021-10-11 01:02:09 -0400

    Make bid property double instead of String.

--- app/src/main/java/io/github/maylcf/coinconverter/data/model/ExchangeResponse.kt
+++ app/src/main/java/io/github/maylcf/coinconverter/data/model/ExchangeResponse.kt
@@ -11,7 +11,7 @@ data class ExchangeResponseValue(
     val low: String,
     val varBid: String,
     val pctChange: String,
-    val bid: String,
+    val bid: Double,
     val ask: String,
     val timestamp: String,
     val createDate: String
